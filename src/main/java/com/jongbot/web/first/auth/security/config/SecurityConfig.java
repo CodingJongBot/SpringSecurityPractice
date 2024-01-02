a08ee8bf3499beaafd5a3b247d2af485ae3619ac
LeeJongwon <LeeJongwon@218.38.137.28>
2024-01-03 01:20:20 +0900

    apply thymeleaf templates

--- src/main/java/com/jongbot/web/first/auth/security/config/SecurityConfig.java
+++ src/main/java/com/jongbot/web/first/auth/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
-package com.jongbot.web.first.auth.security;
+package com.jongbot.web.first.auth.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,67 +19,54 @@ import org.springframework.security.web.authentication.ui.DefaultLoginPageGenera
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity(debug = true)
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter;
-//	DefaultLoginPageGeneratingFilter defaultLoginPageGeneratingFilter;
-//	
+	
 //	private BCryptPasswordEncoder passwordEncoder;
 //		
 //	@Autowired
 //	public SecurityConfig(BCryptPasswordEncoder passwordEncoder) {
 //		this.passwordEncoder = passwordEncoder;
 //	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-		.withUser(
-				User.
-				withUsername("User1")
-				.password(passwordEncoder().encode("1111"))
-				.roles("USER")
-				.build()
-				)
-		.withUser(
-				User
-				.withUsername("Admin")
-				.password(passwordEncoder().encode("2222"))
-				.roles("ADMIN")
-				.build()
-				)
-		;
+				.withUser(User.withUsername("User1").password(passwordEncoder().encode("1111")).roles("USER").build())
+				.withUser(User.withUsername("Admin").password(passwordEncoder().encode("2222")).roles("ADMIN").build());
 	}
-	
+
 	@Override
-	public  void configure(WebSecurity web) throws Exception{
-		web.ignoring()
-			.antMatchers("/assets/**")
-			.antMatchers("/static/**");
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.headers().disable()
-		.csrf().disable()
-		.authorizeRequests()
-			.antMatchers("/user/*").hasRole("USER")
-			.antMatchers("/admin/*").hasRole("ADMIN")
-			.anyRequest().authenticated()
+			.headers().disable()
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/", "/login").permitAll()
+			.antMatchers("/teacher/*").hasRole("ADMIN")
+				.anyRequest().authenticated()
 		.and()
-		
-		.formLogin()
-//			.loginPage("/login")
-//			.successForwardUrl("/loginTest")
-//			.loginProcessingUrl("/user/loginSuccess")
-			.defaultSuccessUrl("/user/loginSuccess",false)
-			.permitAll();
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.defaultSuccessUrl("/", false)//			.defaultSuccessUrl("/user/loginSuccess",false)
+				.failureUrl("/login-error")
+		.and()
+			.logout()
+				.logoutSuccessUrl("/")
+		.and()
+			.exceptionHandling()
+				.accessDeniedPage("/access-denied");
 	}
-	
+
 	@Bean
 	BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}	
+	}
 
 }
